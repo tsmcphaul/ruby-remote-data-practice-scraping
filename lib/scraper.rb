2edc254c4d7644170525b798a36d793ee8827353
Tammarra McPhaul <shaemc22@gmail.com>
2021-02-16 09:26:40 -0500

    commit tree 66266e16f6d8c14430e22554c5d25cd6402e905f

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,3 +2,6 @@ require 'nokogiri'
 require 'open-uri'
 
 html = open("https://flatironschool.com/")
+doc = Nokogiri::HTML(html) 
+ doc.css(".site-header__hero__headline").text.strip
+ puts doc.css(".site-header__hero__headline") 
